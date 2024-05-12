34b90eb5ddee206c51a4b072ff26096de7c9a27b
xkx2021 <89621972+xkx2021@users.noreply.github.com>
2024-05-12 20:33:38 +0800

    Update buildmoon.sh for arm

--- buildmoon.sh
+++ buildmoon.sh
@@ -5,5 +5,5 @@ if [ ! -d "ZeroTierOne" ] ; then
 fi
 SOURCE_ROOT="./ZeroTierOne"
 
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp gcc:4.9 \
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp arm64v8/gcc:4.9 \
 c++ -std=c++11 -I${SOURCE_ROOT} -I${SOURCE_ROOT}/attic -I${SOURCE_ROOT}/ext -g -o mkmoonworld ${SOURCE_ROOT}/node/C25519.cpp ${SOURCE_ROOT}/node/Salsa20.cpp ${SOURCE_ROOT}/node/SHA512.cpp ${SOURCE_ROOT}/node/Identity.cpp ${SOURCE_ROOT}/node/Utils.cpp ${SOURCE_ROOT}/node/InetAddress.cpp ${SOURCE_ROOT}/osdep/OSUtils.cpp mkmoonworld.cpp -lm -lpthread -static
